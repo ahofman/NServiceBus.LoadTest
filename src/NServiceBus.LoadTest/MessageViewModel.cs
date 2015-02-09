e4f466900ce4f3095f0945fd5a9b390679421ad3
ahofman <adrian@adrianhofman.com>
2015-02-09 14:15:42 +1100

    Initial commit 3

---
+++ src/NServiceBus.LoadTest/MessageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NServiceBus.LoadTest
+{
+    public class MessageViewModel
+    {
+        public IEnumerable<PropertyModel> Properties { get; set; }
+    }
+}
---
+++ src/NServiceBus.LoadTest/PropertyModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NServiceBus.LoadTest
+{
+    public class PropertyModel
+    {
+        public PropertyInfo PropertyInfo { get; set; }
+
+
+    }
+}
